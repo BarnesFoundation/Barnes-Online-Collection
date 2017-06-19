4766b74581ec4a472775f66b2163d15d696df221
Cassie Tarakajian <ctarakajian@gmail.com>
2017-06-19 16:39:20 -0400

    update readme

--- src/reducers/objects.js
+++ src/reducers/objects.js
@@ -1,12 +1,13 @@
 import * as ActionTypes from '../constants';
 
 const AWS_BUCKET = process.env.REACT_APP_AWS_BUCKET;
+const AWS_PREFIX = process.env.REACT_APP_IMAGES_PREFIX;
 
 export const generateObjectImageUrls = (object) => {
   if (!object.imageSecret) {
     return object;
   }
-  const baseUrl = `https://s3.amazonaws.com/${AWS_BUCKET}/images`;
+  const baseUrl = `https://s3.amazonaws.com/${AWS_BUCKET}/${AWS_PREFIX}`;
   const newObject = Object.assign({}, object);
   newObject.imageUrlSmall = `${baseUrl}/${object.id}_${object.imageSecret}_n.jpg`;
   newObject.imageUrlLarge = `${baseUrl}/${object.id}_${object.imageSecret}_b.jpg`;
