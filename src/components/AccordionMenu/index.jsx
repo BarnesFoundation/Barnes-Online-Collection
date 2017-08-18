2f011fd0d98d1eac5529699646773c61b2e3ed18
Eric Chaves <erchaves@gmail.com>
2017-08-18 18:09:26 -0400

    updating accordion styles

--- src/components/AccordionMenu/index.jsx
+++ src/components/AccordionMenu/index.jsx
@@ -27,47 +27,51 @@ class AccordionMenu extends Component {
 
   render() {
     return (
-      <div>
-        <div className="m-accordion-tabs" data-behavior="Accordion" role="tablist" data-close-others="true">
-          {
-            this.props.tabList
-              .map((tabData, idx) => {
-                const isSelected = idx === this.state.selectedIdx;
+      <div className="component-m-support-accordion">
+        {
+          this.props.tabList
+            .map((tabData, idx) => {
+              const isSelected = idx === this.state.selectedIdx;
 
-                const props = {
-                  ...tabData,
-                  idx: idx,
-                  isSelected: isSelected,
-                  handleContentTabClick: this.handleContentTabClick,
-                };
+              const props = {
+                ...tabData,
+                idx: idx,
+                isSelected: isSelected,
+                handleContentTabClick: this.handleContentTabClick,
+              };
 
-                return (
-                  <div>
-                    <button type="button"
-                      className="m-accordion-tabs__title font-delta"
-                      aria-selected={isSelected}
-                      role="tab"
-                      tabIndex={idx}
-                      aria-expanded={isSelected}
-                      onClick={this.handleContentTabClick(idx)}
-                    >
+              return (
+                <div className="m-block m-block--shallow m-support-accordion">
+                  <button type="button"
+                    className="font-delta m-block__shallow-title m-support-accordion__title"
+                    aria-selected={isSelected}
+                    role="tab"
+                    tabIndex={idx}
+                    aria-expanded={isSelected}
+                    onClick={this.handleContentTabClick(idx)}
+                  >
+                    <h3>
                       {tabData.title}
-                      <span className="m-accordion-tabs__icon"><svg height={20} width={20}><use xlinkHref="#icon--icon_arrow_down" /></svg></span>
-                    </button>
-                    <div
-                      className="m-accordion-tabs__body"
-                      aria-labelledby={`accordion1494${idx}`}
-                      aria-hidden={!isSelected}
-                      role="tabpanel"
-                      data-height=""
-                    >
-                      <tabData.contentBlock {...props}/>
-                    </div>
+                    </h3>
+                    <span className="m-support-accordion__btn">
+                      <span className="m-support-accordion__toggle--show">Show details</span>
+                      <span className="m-support-accordion__toggle--hide">Hide details</span>
+                    </span>
+                  </button>
+                  <div
+                    className="m-support-accordion__body"
+                    aria-labelledby={`accordion1494${idx}`}
+                    aria-hidden={!isSelected}
+                    role="tabpanel"
+                    data-height=""
+                  >
+                    test
+                    <tabData.contentBlock {...props}/>
                   </div>
-                )
-              })
-          }
-        </div>
+                </div>
+              )
+            })
+        }
       </div>
     )
   }
