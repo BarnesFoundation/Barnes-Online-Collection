45ce32a5a95a91ad519937c70cdfc2d28424ef38
Eric Chaves <erchaves@gmail.com>
2017-09-08 17:18:51 -0400

    adding cross browser slider component

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import './slider.css';
 
+import SliderEl from 'rc-slider/lib/Slider';
+import 'rc-slider/assets/index.css';
+
+const trackHeight = 5;
+const handleSize = 30;
+const trackColor = '#b4b4b4';
+const handleBorderColor = '#6e6e6e';
+const trackStyle = {
+  backgroundColor: trackColor,
+  height: trackHeight,
+};
+
 class Slider extends Component {
   constructor(props) {
     super(props);
-  }
 
-  static defaultProps = {
-    rangeMin: 0,
-    rangeMax: 100,
-    defaultValue: 50,
+    this.state = {
+      value: this.props.defaultValue || 50,
+    };
   }
 
   render() {
@@ -18,18 +28,24 @@ class Slider extends Component {
         className="component-slider"
         data-label-style={this.props.labelStyle}
       >
-        <span
-          className="slider-label font-smallprint hidden show-for-style-inline"
-        >{this.props.labelLeft}</span>
-        <input
+       <span
+        className="slider-label slider-label-left font-smallprint hidden show-for-style-inline"
+      >{this.props.labelLeft}</span>
+        <SliderEl
           className="slider"
-          type="range"
-          min={this.props.rangeMin}
-          max={this.props.rangeMax}
-          defaultValue={this.props.defaultValue}
+          defaultValue={this.state.value}
+          trackStyle={trackStyle}
+          railStyle={trackStyle}
+          handleStyle={{
+            borderColor: handleBorderColor,
+            height: handleSize,
+            width: handleSize,
+            marginLeft: -handleSize/2,
+            marginTop: -handleSize/2 + trackHeight/2,
+          }}
         />
-        <span className="slider-label font-smallprint hide-for-style-inline">{this.props.labelLeft}</span>
-        <span className="slider-label font-smallprint">{this.props.labelRight}</span>
+        <span className="slider-label slider-label-left font-smallprint hide-for-style-inline">{this.props.labelLeft}</span>
+        <span className="slider-label slider-label-right font-smallprint">{this.props.labelRight}</span>
       </div>
     );
   }
