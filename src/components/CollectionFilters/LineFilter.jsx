1003634170562a9361e6721bfb59e04e63bc422b
rnackman <rachel.nackman@gmail.com>
2017-09-12 16:29:34 -0400

    Use Icon component in place of SVG.

--- src/components/CollectionFilters/LineFilter.jsx
+++ src/components/CollectionFilters/LineFilter.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 
-// import { bindActionCreators } from 'redux';
-// import { connect } from 'react-redux';
-// import * as FiltersActions from '../../actions/filters';
-// import * as FilterSetsActions from '../../actions/filterSets';
+import Icon from '../Icon';
 
 class LineFilter extends Component {
   constructor(props) {
@@ -11,70 +8,22 @@ class LineFilter extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  // filterIsApplied() {
-  //   const filters = this.props.filters;
-  //   for (let i = 0; i < filters.length; i++) {
-  //     if (filters[i].slug === this.props.filter.slug) {
-  //       return i;
-  //     }
-  //   }
-  //   return -1;
-  // }
-
   handleClick(event) {
     event.preventDefault();
     this.props.handleClick();
-    // const filter = this.props.filter;
-    // if (this.props.filters.length === 0) {
-    //   this.props.addToFilters(filter);
-    // } else {
-    //   const index = this.filterIsApplied();
-    //   if (index === -1) {
-    //     this.props.addToFilters(filter);
-    //   } else {
-    //     this.props.removeFilterByIndex(index);
-    //   }
-    // }
   }
-
-  // getClasses() {
-  //   const filter = this.props.filter;
-  //   let classes = 'btn line-filter font-smallprint';
-  //   if (this.filterIsApplied() > -1) {
-  //     classes += ' is-applied';
-  //   }
-  //   return classes;
-  // }
 
   render() {
     return (
       <button
         onClick={this.handleClick}
-        // className={this.getClasses()}
         className={this.props.classes}
       >
-        <svg className={`icon icon-${this.props.svgId} collection-filter-icon`}>
-          <use xlinkHref={`#icon-${this.props.svgId}`}></use>
-        </svg>
+        <Icon svgId={this.props.svgId} classes='collection-filter-icon' />
         {this.props.name}
       </button>
     );
   }
 }
-
-// const mapStateToProps = state => {
-//   return {
-//     filters: state.filters,
-//   }
-// }
-
-// const mapDispatchToProps = dispatch => {
-//   return bindActionCreators(Object.assign({},
-//     FiltersActions,
-//     FilterSetsActions,
-//   ), dispatch);
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(LineFilter);
 
 export default LineFilter;
