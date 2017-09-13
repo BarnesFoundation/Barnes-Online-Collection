0c8d8091aa674ebd704fa234bfb65c15ef0114d5
Micah Walter <micah@micahwalter.com>
2017-09-13 13:10:12 -0400

    deploy with root

--- ubuntu/post-deploy.sh
+++ ubuntu/post-deploy.sh
@@ -4,8 +4,8 @@ export HOME=/home/ubuntu
 
 cd /usr/local/barnes/projects/barnes-collection-www
 
-npm i -g npm
-npm install
-npm run build
+sudo npm i -g npm
+sudo npm install
+sudo npm run build
 
-pm2 restart "Barnes Collection Website"
+sudo pm2 restart "Barnes Collection Website"
