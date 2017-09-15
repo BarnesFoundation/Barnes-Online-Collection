814344dab2cb9358465c24f640b299b8442c28eb
rnackman <rachel.nackman@gmail.com>
2017-09-15 15:41:50 -0400

    Adjust Barnesian algorithm to incorporate knick knacks.

--- src/actions/objects.js
+++ src/actions/objects.js
@@ -5,9 +5,9 @@ import * as ActionTypes from '../constants';
 const BARNES_SETTINGS = {
   min2D: 10,
   minMetalworks: 7,
-  minObjects: 7,
   min3D: 5,
-  minKnickKnacks: 5,
+  minKnickKnacks: 2,
+  minObjects: 14,
   terms2D: ['Architecture', 'Paintings', 'Drawings', 'Works on Paper', 'Prints', 'Enamels', 'Manuscripts', 'Photographs'],
   termsMetalworks: ['Metalworks'],
   terms3D: ['Sculptures', 'Furniture', 'Timepieces'],
@@ -73,7 +73,8 @@ const barnesifyObjects = (objects, dispatch, options) => {
     twoD: [],
     metalworks: [],
     threeD: [],
-    objects: []
+    objects: [],
+    knickknacks: []
   };
 
   const updateBarnesObjects = (objects, type) => {
@@ -92,17 +93,33 @@ const barnesifyObjects = (objects, dispatch, options) => {
     console.log('Compiling Barnesified object set...');
     let refinedBarnesObjects = barnesObjects.twoD.slice(0, BARNES_SETTINGS.min2D);
     ratios['2D'] = refinedBarnesObjects.length;
+    let metalworks, threeD, knickknacks;
 
     if (barnesObjects.metalworks.length >= BARNES_SETTINGS.minMetalworks) {
-      refinedBarnesObjects.push(...barnesObjects.metalworks.slice(0, BARNES_SETTINGS.minMetalworks));
-      ratios['metalworks'] = refinedBarnesObjects.length - ratios['2D'];
-
-      refinedBarnesObjects.push(...barnesObjects.threeD.slice(0, BARNES_SETTINGS.size - refinedBarnesObjects.length));
-      ratios['3D'] = refinedBarnesObjects.length - ratios['2D'] - ratios['metalworks'];
+      metalworks = barnesObjects.metalworks.slice(0, BARNES_SETTINGS.minMetalworks);
     } else {
-      refinedBarnesObjects.push(...barnesObjects.objects.slice(0, BARNES_SETTINGS.size - refinedBarnesObjects.length));
-      ratios['3D+Metalworks'] = refinedBarnesObjects.length - ratios['2D'];
+      metalworks = barnesObjects.metalworks;
     }
+    refinedBarnesObjects.push(...metalworks);
+    ratios['metalworks'] = metalworks.length;
+    let metalworksDiff = BARNES_SETTINGS.minMetalworks - metalworks.length;
+
+    if (barnesObjects.threeD.length >= BARNES_SETTINGS.min3D) {
+      threeD = barnesObjects.threeD.slice(0, BARNES_SETTINGS.min3D + metalworksDiff);
+    } else {
+      threeD = barnesObjects.threeD;
+    }
+    refinedBarnesObjects.push(...threeD);
+    ratios['3D'] = threeD.length;
+    let threeDDiff = BARNES_SETTINGS.min3D - threeD.length;
+
+    if (barnesObjects.knickknacks.length >= BARNES_SETTINGS.knickknacks) {
+      knickknacks = barnesObjects.knickknacks.slice(0, BARNES_SETTINGS.minKnickKnacks + threeDDiff);
+    } else {
+      knickknacks = barnesObjects.knickknacks;
+    }
+    refinedBarnesObjects.push(...knickknacks);
+    ratios['knickknacks'] = knickknacks.length;
 
     ratios['total'] = refinedBarnesObjects.length;
 
@@ -112,7 +129,7 @@ const barnesifyObjects = (objects, dispatch, options) => {
     console.log('2D:', ratios['2D'], 'objects', '/', ratios['2D']/ratios.total);
     console.log('metalworks:', ratios['metalworks'], 'objects', '/', ratios['metalworks']/ratios.total);
     console.log('3D:', ratios['3D'], 'objects', '/', ratios['3D']/ratios.total);
-    console.log('3D+Metalworks:', ratios['3D+Metalworks'], 'objects', '/',  ratios['3D+Metalworks']/ratios.total);
+    console.log('Knick Knacks:', ratios['knickknacks'], 'objects', '/',  ratios['knickknacks']/ratios.total);
 
     dispatch(setObjects(objects));
   }
@@ -128,6 +145,26 @@ const barnesifyObjects = (objects, dispatch, options) => {
     return { params: { body: body } };
   }
 
+  const checkBarnesificationPossible = () => {
+    if (barnesObjects.metalworks.length >= BARNES_SETTINGS.minMetalworks) {
+      if (barnesObjects.threeD.length >= BARNES_SETTINGS.min3D) {
+        return true;
+      } else {
+        if ((barnesObjects.threeD.length + barnesObjects.knicknacks.length) >= (BARNES_SETTINGS.min3D + BARNES_SETTINGS.minKnickKnacks)) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    } else {
+      if ((barnesObjects.threeD.length + barnesObjects.metalworks.length) >= (BARNES_SETTINGS.minMetalworks + BARNES_SETTINGS.min3D)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
   axios.get('/api/search', params(BARNES_SETTINGS.terms2D))
   .then((response) => {
     if (response.data.hits.total >= BARNES_SETTINGS.min2D) {
@@ -136,28 +173,25 @@ const barnesifyObjects = (objects, dispatch, options) => {
 
       axios.get('/api/search', params(BARNES_SETTINGS.termsMetalworks))
       .then((response) => {
-        if (response.data.hits.total >= BARNES_SETTINGS.minMetalworks) {
-          console.log('Retrieved', response.data.hits.total, 'metalworks. Proceeding...');
-          updateBarnesObjects(response.data.hits.hits, 'metalworks');
+        console.log('Retrieved', response.data.hits.total, 'metalworks. Proceeding...');
+        updateBarnesObjects(response.data.hits.hits, 'metalworks');
 
-          axios.get('/api/search', params(BARNES_SETTINGS.terms3D))
+        axios.get('/api/search', params(BARNES_SETTINGS.terms3D))
+        .then((response) => {
+          console.log('Retrieved', response.data.hits.total, '3D objects. Proceeding...');
+          updateBarnesObjects(response.data.hits.hits, 'threeD');
+          axios.get('/api/search', params(BARNES_SETTINGS.termsKnickKnacks))
           .then((response) => {
-            console.log('Retrieved', response.data.hits.total, '3D objects. Proceeding...');
-            updateBarnesObjects(response.data.hits.hits, 'threeD');
-            setBarnesObjects();
-          })
-        } else {
-          axios.get('/api/search', params(['Metalworks', ...BARNES_SETTINGS.terms3D]))
-          .then((response) => {
-            if (response.data.hits.total >= BARNES_SETTINGS.minObjects) {
-              console.log('Retrieved', response.data.hits.total, 'metalworks + 3D objects. Proceeding...');
-              updateBarnesObjects(response.data.hits.hits, 'objects');
+            console.log('Retrieved', response.data.hits.total, 'knickknacks. Proceeding...');
+            updateBarnesObjects(response.data.hits.hits, 'knickknacks');
+
+            if (checkBarnesificationPossible()) {
               setBarnesObjects();
             } else {
               dispatch(setObjects(objects));
             }
-          });
-        }
+          })
+        });
       });
     } else {
       console.log('Not enough 2D objects to Barnesify. Aborting...')
