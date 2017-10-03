3f39e8075d4dc0ea0cc362cc13f78b3bb3cb5f44
Eric Chaves <erchaves@gmail.com>
2017-10-02 20:26:06 -0400

    adding clear button to filters per https://trello.com/c/nVs84Pk2/234-collection-home-clear-all-option-missing-from-filter-search-on-desktop

--- src/components/SearchInput/SearchApplied.jsx
+++ src/components/SearchInput/SearchApplied.jsx
@@ -2,29 +2,14 @@ import React, { Component } from 'react';
 
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-
-import * as SearchActions from '../../actions/search';
-import * as ObjectsActions from '../../actions/objects';
+import ClearAllButton from './ClearAllButton'
 
 class SearchApplied extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.props.clearSearchTerm();
-    this.props.getAllObjects();
-  }
-
   render() {
     return (
       <div className="search-results">
         <p>Results for "{this.props.search}"</p>
-        <button
-          onClick={this.handleClick}
-          className="btn">Clear all</button>
+        <ClearAllButton />
       </div>
     );
   }
@@ -38,8 +23,6 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators(Object.assign({},
-    SearchActions,
-    ObjectsActions
   ), dispatch);
 }
 
