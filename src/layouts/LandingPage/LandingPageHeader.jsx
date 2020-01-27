a5fbabb06ccb205d77f7de016fc5201063eab7e7
agallagher777 <agallagher@barnesfoundation.org>
2020-01-27 13:32:45 -0500

    Adding header styling.

--- src/layouts/LandingPage/LandingPageHeader.jsx
+++ src/layouts/LandingPage/LandingPageHeader.jsx
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 class LandingPageHeader extends Component {
   render() {
     return (
-      <div className='landing-page-image'>
-        <h2 className='landing-page-header font-gamma'>The minute you step into the galleries of the Barnes collection, you know you're in for an experience like no other.</h2>
+      <div className='o-hero o-hero--landing-page'>
+        <div className='o-hero__inner'>
+          <div className='container o-hero__container'>
+            <div className='o-hero__copy'>
+              <p className='o-hero__supporting'>The minute you step into the galleries of the Barnes collection, you know you're in for an experience like no other.</p>
+            </div>
+          </div>
+        </div>
       </div>
       // <h2 className="landing-page-header h2 font-gamma">Albert Barnes taught people to look at works <br className="large-only"/>of art primarily in terms of their visual relationships.</h2>
     );
