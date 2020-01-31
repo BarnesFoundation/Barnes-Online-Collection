6a0eb32e361efdd4c57767ac0bf5bfcbd68a3aa1
agallagher777 <agallagher@barnesfoundation.org>
2020-01-31 10:53:48 -0500

    Adding selectable images below hero image.

--- src/components/ArtObjectPageComponents/PanelDetails/index.jsx
+++ src/components/ArtObjectPageComponents/PanelDetails/index.jsx
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-import './index.css';
-
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import SummaryTable from './SummaryTable';
+import AccordionMenu from '../../AccordionMenu';
+import Zoom from '../../Zoom/Zoom';
+import Icon from '../../Icon'
 import * as ObjectActions from '../../../actions/object';
 import * as PrintActions from '../../../actions/prints';
-import AccordionMenu from '../../../components/AccordionMenu';
-import SummaryTable from './SummaryTable';
-import Zoom from '../../../components/Zoom/Zoom';
 import { getObjectCopyright } from '../../../copyrightMap';
+import './index.css';
 
 // use JSON.parse to parse string "true" or "false"
 const isZoomEnabled = process.env.REACT_APP_FEATURE_ZOOMABLE_IMAGE && JSON.parse(process.env.REACT_APP_FEATURE_ZOOMABLE_IMAGE);
 
-const getTabList = (artObjectProps) => {
-  return [
+const getTabList = (artObjectProps) => (
+  [
     {
       title: 'Long Description',
       tabContent: artObjectProps.longDescription,
@@ -26,23 +26,99 @@ const getTabList = (artObjectProps) => {
     {
       title: 'Exhibition History',
       tabContent: artObjectProps.exhHistory,
-	},
-	{
-		title: 'Provenance',
-		tabContent: artObjectProps.publishedProvenance
-	}
-  ].filter((obj) => {
-    // filter out ones with no content
-    return !!obj.tabContent;
-  });
+    },
+    {
+      title: 'Provenance',
+      tabContent: artObjectProps.publishedProvenance
+    }
+  ].filter(Boolean) // Filter out tabs with no content.
+);
+
+// TODO => Convert this to class if smaller images end up being available on object level.
+const Image = ({
+  isZoomed,
+  onLoad,
+  setRef,
+  isLoaded,
+  object,
+  width,
+  activeImageIndex,
+  setActiveImageIndex
+}) => {
+  let className = 'art-object__image';
+  if (isZoomed) className = `${className} art-object__image-hidden`;
+
+  const actualWidth = width || '100%';
+
+  return (
+    <div className='art-object__header m-block'>
+      {isZoomed && <Zoom id={object.id} />}
+      {!isZoomed && <img
+        aria-hidden="true"
+        className=""
+        src={object.imageUrlLarge}
+        alt={object.title}
+        onLoad={onLoad}
+        ref={setRef}
+      />}
+      {Boolean((isLoaded && width) || isZoomed) &&
+        <div className='image-caption'>
+          <div
+            className='font-smallprint color-medium image-caption__content'
+            style={{ width: actualWidth }}
+          >
+            {object.people}. {object.title}, {object.displayDate}. {object.invno}. {object.creditLine}
+          </div>
+          <div
+            className='image-caption__grid'
+            style={{ width: actualWidth }}
+          >
+            {/* TODO => Replace this with real images. */}
+            {[...Array(8)].map((x, i) => {
+              let className = 'image-caption__grid-image';
+              console.log(activeImageIndex);
+              if (activeImageIndex === i) className = `${className} image-caption__grid-image--active`;
+
+              return (
+                <div
+                  onClick={() => setActiveImageIndex(i)}
+                  className={className}
+                  key={i}
+                >
+                  <img src={object.imageUrlSmall} />
+                  <div className='image-caption__inner-border'></div>
+                </div>
+              )
+            })}
+          </div>
+        </div>}
+    </div>
+  );
 };
 
 class PanelDetails extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.ref = null;
+    this.state = {
+      imageLoaded: false,
+      activeImageIndex: 0,
+    };
+  }
+
+  /** Update state infomration. */
+  onLoad = () => this.setState({ imageLoaded: true });
+  setActiveImageIndex = activeImageIndex => this.setState({ activeImageIndex }); 
+
+  /** Ref to determine width of caption and images. */
+  setRef = ref => this.ref = ref;
+
   render() {
-    const object = this.props.object;
-    const printAvailable = this.props.prints.find((print) => {
-      return print.id === object.invno
-    });
+    const { object, prints } = this.props;
+    const { imageLoaded, activeImageIndex } = this.state;
+
+    const printAvailable = prints.find(({ id }) => id === object.invno);
 
     const objectCopyrightDetails = getObjectCopyright(object);
     const accordionTabList = getTabList(object);
@@ -52,67 +128,59 @@ class PanelDetails extends Component {
 
     return (
       <div className="art-object-page__panel-details">
-        <div className="art-object__header m-block">
-          {(
-            objectCopyrightDetails.type === "large" &&
-            isZoomEnabled
-          ) ?
-            <div>
-              <Zoom id={object.id} />
-              <img
-                aria-hidden="true"
-                className="art-object__image art-object__image-hidden"
-                src={object.imageUrlLarge}
-                alt={object.title}
-              />
-            </div>
-          :
-            <div className="container-inner-narrow">
-              <img className="art-object__image" src={object.imageUrlLarge} alt={object.title}/>
-            </div>
-          }
-        </div>
+        <Image
+          onLoad={this.onLoad}
+          isLoaded={imageLoaded}
+          setRef={this.setRef}
+          width={this.ref ? this.ref.width : 0}
+          object={object}
+          isZoomed={Boolean(objectCopyrightDetails.type === 'large' && isZoomEnabled)}
+          activeImageIndex={activeImageIndex}
+          setActiveImageIndex={this.setActiveImageIndex}
+        />
         <div className="art-object__more-info m-block m-block--shallow">
           <div className="container-inner-narrow">
             <SummaryTable {...object} objectCopyrightDetails={objectCopyrightDetails}/>
 
             {/* Removed rel="noopener noreferrer nofollow" from the following links. */}
             <div className="m-block m-block--no-border m-block--shallow m-block--flush-top">
-              {objectCopyrightDetails.type === "large" ?
+              {objectCopyrightDetails.type === "large"
+              ?
                 <a className="btn btn--primary" href={downloadRequestUrl} target="_blank" >
                   Download Image
                 </a>
               :
                 <a className="btn btn--primary" href={requestImageUrl} target="_blank" >
                   Request Image
-                </a>
-              }
+                </a>}
 
               {printAvailable &&
                 <a className="btn" href={printAvailable.url} target="_blank" >
-                Purchase Print
-                </a>
-              }
+                  Purchase Print
+                </a>}
+              <div className='share-button'>
+                <div className='share-button__content'>
+                  <div className='share-button__icon' >
+                    <Icon svgId='-icon_share' classes='share-button__svg'/>
+                  </div>
+                  <span className='font-simple-heading share-button__text'>Share It</span>
+                </div>
+              </div>
             </div>
 
-            {
-              object.shortDescription &&
+            {object.shortDescription &&
               <div className="art-object__more-info m-block m-block--shallow">
-                <div className="art-object__short-description" dangerouslySetInnerHTML={{__html: object.shortDescription}}>
-				</div>
-              </div>
-            }
+                <div className="art-object__short-description" dangerouslySetInnerHTML={{ __html: object.shortDescription }}>
+				        </div>
+              </div>}
 
-            {
-              accordionTabList.length > 0 &&
-              <AccordionMenu tabList={accordionTabList} />
-            }
+            {Boolean(accordionTabList) && <AccordionMenu tabList={accordionTabList} />}
           </div>
         </div>
       </div>
     );
   }
-}
+};
 
 
 function mapStateToProps(state) {
--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-export default ({ svgId, classes }) => (
-  <svg className={`icon icon-${svgId} ${classes}`}>
-    <use xlinkHref={`#icon-${svgId}`}></use>
-  </svg>
-);
+export default ({ svgId, classes }) => {
+  let className = `icon icon-${svgId}`;
+  if (classes) className = `${className} ${classes}`;
+
+  return (
+    <svg className={className}>
+      <use xlinkHref={`#icon-${svgId}`}></use>
+    </svg>
+  );
+}
