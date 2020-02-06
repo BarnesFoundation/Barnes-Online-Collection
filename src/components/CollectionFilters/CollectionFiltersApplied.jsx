48db837be6e75469ca3a0c0a3e1c480008624bd8
agallagher777 <agallagher@barnesfoundation.org>
2020-02-06 10:03:50 -0500

    Fixing dropdown selection and applied filters.

--- src/components/CollectionFilters/CollectionFiltersApplied.jsx
+++ src/components/CollectionFilters/CollectionFiltersApplied.jsx
@@ -1,48 +1,31 @@
 import React, { Component } from 'react';
-
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import ClearAllButton from '../SearchInput/ClearAllButton'
 import MobilePanelShuffleButton from './MobilePanelShuffleButton'
 import FilterTag from './FilterTag';
-
 import * as FiltersActions from '../../actions/filters';
 
-class CollectionFiltersApplied extends Component {
-  getFilterTags(filters) {
-    return filters.map((filter, index) =>
-      <FilterTag
-        key={index}
-        index={index}
-        filter={filter}
-      />
-    );
-  }
-
-  render() {
-    const filters = this.props.filters.ordered;
-
-    if (!filters) {
-      return null;
-    }
-
-    const hasFilters = filters.length > 0;
-
-    return ( hasFilters &&
-      <div className="applied-filter-tags-container-wrap">
-        <div className="flex-left">
-          <div className="applied-filter-tags-container">
-            {this.getFilterTags(filters)}
-          </div>
-        </div>
-        <div className="flex-right">
-          <ClearAllButton />
-          <MobilePanelShuffleButton />
+const CollectionFiltersApplied = ({ filters: { ordered }}) => (
+  Boolean(ordered.length) && 
+    <div className="applied-filter-tags-container-wrap">
+      <div className="flex-left">
+        <div className="applied-filter-tags-container">
+          {ordered.map((filter, index) => (
+            <FilterTag
+              key={index}
+              index={index}
+              filter={filter}
+            />)
+          )}
         </div>
       </div>
-    );
-  }
-}
+      <div className="flex-right">
+        <ClearAllButton />
+        <MobilePanelShuffleButton />
+      </div>
+    </div>
+);
 
 const mapStateToProps = state => {
   return {
@@ -54,6 +37,6 @@ const mapDispatchToProps = dispatch => {
   return bindActionCreators(Object.assign({},
     FiltersActions,
   ), dispatch);
-}
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(CollectionFiltersApplied);
--- src/components/SearchInput/Dropdowns.jsx
+++ src/components/SearchInput/Dropdowns.jsx
@@ -1,48 +1,115 @@
 import React, { Component } from 'react';
 import Icon from '../Icon';
-import './dropdownsMenu.css';
+import assets from '../../constants/searchAssets.json';
+import './dropdowns.css';
 
-const DROPDOWN_TERMS = ['Culture', 'Year', 'Medium', 'Colors', 'Copyright', 'Artist']; 
-
-const getDropdownContent = (term) => {
-    switch(term) {
-        case 1: {
-            return 
-        }
-    }
+const DROPDOWN_TERMS = ['Culture', 'Year', 'Medium', 'Location', 'Copyright', 'Artist'];
+const DROPDOWN_TERMS_MAP = {
+    'Culture': assets.cultures,
+    'Year': [],
+    'Medium': [], // TODO => populate this.
+    'Location': assets.locations, // 
+    'Copyright': assets.copyrights,
+    'Artist': assets.artists,
 };
 
-const Dropdown = ({ children }) => {
-    return (
-        <div className='dropdown'>
-            {children}
-        </div>
-    );
-};
+/**
+ * Regular dropdown list.
+ * @see getDropdownContent
+ * */
+const DropdownList = ({ data, activeTerms, setActiveTerm }) => (
+    <ul className='dropdown__list'>
+        {data
+            .filter(({ key }) => key) // Filter out null items.
+            .map(({ key }) => {
+                const isActiveItem = activeTerms.includes(key);
+
+                let listItemClassNames = 'dropdown__list-item';
+                if (activeTerms.includes(key)) listItemClassNames = `${listItemClassNames} ${listItemClassNames}--active`;
+
+                return (
+                    <li
+                        key={key}
+                        className={listItemClassNames}
+                        onClick={() => setActiveTerm(key)}
+                    >
+                        <span>{key}</span>
+                        {isActiveItem && <Icon svgId='-icon_close' classes='dropdown__icon' />}
+                    </li>
+                );
+            }
+        )}
+    </ul>
+);
 
 /** Dropdown menu for filtering artwork. */
 export class Dropdowns extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            activeItem: null
+            activeItem: null,
+            dropdownContent: null,
+            activeTerms: [],
         };
     };
 
     /**
      * Set active index to item clicked, if this is already the active index then reset to null.
-     * @param {number} index - index of clicked item.
+     * @param {string} index - index of clicked item.
      */
-    setActiveItem(index) {
+    setActiveItem(term) {
         const { activeItem } = this.state;
 
         // If this is the current active item, reset to null.
-        if (activeItem === index) {
+        if (activeItem === term) {
             this.setState({ activeItem: null });
         } else {
-            this.setState({ activeItem: index });
+            this.setState({ activeItem: term });
         }
     }
+
+    /**
+     * Add or remove selected term to filters.
+     * @param {string} term - filter search term.
+     */
+    setActiveTerm = (term) => {
+        const { activeTerms } = this.state;
+
+        // If we are removing an item, filter it out of the array, otherwise append it to the array.
+        if (activeTerms.includes(term)) {
+            this.setState({ activeTerms: activeTerms.filter(activeTerm => activeTerm !== term) });
+        } else {
+            this.setState({ activeTerms: [...activeTerms, term] });
+        }
+    };
+
+    /**
+     * Get inner content for dropdown.
+     * @param {string} term - name of clicked item.
+     * @returns {JSX.Element} JSX to be rendered inside of Dropdown.
+     */
+    getDropdownContent = () => {
+        const { activeItem, activeTerms } = this.state;
+        const data = DROPDOWN_TERMS_MAP[activeItem];
+
+        switch (activeItem) {
+            case('Year'): {
+                return <span>Lorem Ipsum</span>;
+            };
+            case('Colors'): {
+                return <span>Lorem Ipsum</span>;
+            };
+            default: {
+                return (
+                    <DropdownList
+                        data={data}
+                        activeTerms={activeTerms}
+                        setActiveTerm={this.setActiveTerm}
+                    />
+                )
+            }
+        }
+    };
 
     render() {
         const { activeItem } = this.state;
@@ -51,7 +118,7 @@ export class Dropdowns extends Component {
             <div className='dropdowns-menu'>
                 {DROPDOWN_TERMS.map((term, i) => {
                     const isLastDropdown = i === DROPDOWN_TERMS.length - 1;
-                    const isActiveItem = activeItem === i;
+                    const isActiveItem = activeItem === term;
 
                     // If this is the last item, we want to remove the chevron and add a | before the item.
                     let buttonClassName = 'dropdowns-menu__button';
@@ -65,14 +132,27 @@ export class Dropdowns extends Component {
                         <button
                             key={term}
                             className={buttonClassName}
-                            onClick={() => this.setActiveItem(i)}
+                            onClick={() => this.setActiveItem(term)}
                         >
                             <span className='dropdowns-menu__button-content'>{term}</span>
                             {!isLastDropdown && <Icon svgId='-icon_arrow_down' classes={iconClassName} />}
                             {isActiveItem &&
-                                <Dropdown>
-                                    <div>Dropdown content</div>
-                                </Dropdown>
+                                // Prevent propogation of event to deselecting button with onClick.
+                                <div
+                                    className='dropdown'
+                                    onClick={e => {
+                                        e.preventDefault();
+                                        e.stopPropagation();
+                                    }}
+                                >
+                                    <div className='dropdown__header'>
+                                        <span className='font-delta dropdown__header--text'>{term}</span>
+                                        <Icon svgId='-icon_close' classes='dropdown__icon dropdown__icon--x'/>
+                                    </div>
+                                    <div className='dropdown__content'>
+                                        {this.getDropdownContent()}
+                                    </div>
+                                </div>
                             }
                         </button>
                     );
