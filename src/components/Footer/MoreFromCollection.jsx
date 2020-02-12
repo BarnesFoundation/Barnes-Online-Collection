a98ceefa9ed8b572df2524ec9bcb849921dac7e2
Christopher Jativa <cjativa@barnesfoundation.org>
2020-02-12 10:01:06 -0500

    added links for entries

--- src/components/Footer/MoreFromCollection.jsx
+++ src/components/Footer/MoreFromCollection.jsx
@@ -1,47 +1,41 @@
 import React from 'react';
 import axios from 'axios';
-
-// TODO => Delete this, this is for testing layout.
-const MOCK_OBJECT = {
-    title: 'Lorem Ipsum',
-    label: 'Permanent Collection',
-    backgroundImage: 'https://barnesfoundation-collection.imgix.net/5112_U7ZsFncZX0Jq7Xw3_n.jpg',
-    dateField: 'Ongoing',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-};
-const MOCK_OBJECTS = [...Array(3)].map(() => MOCK_OBJECT);
+import { MAIN_WEBSITE_DOMAIN } from '../../constants';
 
 /**
  * Convert object details to JSX card.
  * @param {object} param - Event/More from Collection detail.
  * @returns {JSX.Element} - Event/More from Collection JSX card.
  */
-const MoreFromCollectionCard = ({ moreFromDetail: { title, label, backgroundImage, date, description }}) => (
-    <div className='m-card-event vevent'>
-        <div className='m-card-event__header'>
-            <a className='m-card-event__media-link' href='/'>
-                <img
-                    className='m-card-event__media'
-                    src={backgroundImage}
-                />
-            </a>
-        </div>
-        <div className='m-card-event__body'>
-            <div className='font-zeta m-card-event__type'>{label}</div>
-            <h3 className='font-delta m-card-event__title'>
-                <a href='/'>
-                    {title}
-                </a>
-            </h3>
-            <div className='dtstart font-delta m-card-event__date'>
-                {date}
-            </div>
-            <div className='summary m-card-event__summary'>
-                <p dangerouslySetInnerHTML={{ __html: description}}></p>
-            </div>
-        </div>
-    </div>
-);
+const MoreFromCollectionCard = ({ moreFromDetail: { title, label, backgroundImage, date, description, customSlug } }) => {
+	const entryUrl = `${MAIN_WEBSITE_DOMAIN}/${customSlug}`;
+	return (
+		<div className='m-card-event vevent'>
+			<div className='m-card-event__header'>
+				<a className='m-card-event__media-link' href={entryUrl}>
+					<img
+						className='m-card-event__media'
+						src={backgroundImage}
+					/>
+				</a>
+			</div>
+			<div className='m-card-event__body'>
+				<div className='font-zeta m-card-event__type'>{label}</div>
+				<h3 className='font-delta m-card-event__title'>
+					<a href={entryUrl}>
+						{title}
+					</a>
+				</h3>
+				<div className='dtstart font-delta m-card-event__date'>
+					{date}
+				</div>
+				<div className='summary m-card-event__summary'>
+					<p dangerouslySetInnerHTML={{ __html: description }}></p>
+				</div>
+			</div>
+		</div>
+	)
+};
 
 /**
  * More from collection section.
@@ -70,7 +64,7 @@ export class MoreFromCollection extends React.Component {
 					<div className='m-block'>
 						<h2 className='font-beta m-block__title'>More from the collection</h2>
 						<div className='m-card-event-list'>
-							{(entries || MOCK_OBJECTS).map((entry, i) => <MoreFromCollectionCard key={i} moreFromDetail={entry} />)}
+							{(entries).map((entry, i) => <MoreFromCollectionCard key={i} moreFromDetail={entry} />)}
 						</div>
 					</div>
 				</div>
