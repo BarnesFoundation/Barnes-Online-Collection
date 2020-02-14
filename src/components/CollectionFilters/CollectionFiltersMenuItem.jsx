a964c2e962907899fbc77205747f7b66a068644e
agallagher777 <agallagher@barnesfoundation.org>
2020-02-14 13:41:38 -0500

    Removing MediaQuery from CollectionFiltersMenuItem and converting from class to functional component.

--- src/components/CollectionFilters/CollectionFiltersMenuItem.jsx
+++ src/components/CollectionFilters/CollectionFiltersMenuItem.jsx
@@ -1,76 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import MediaQuery from 'react-responsive';
 import Icon from '../Icon';
-import * as FilterSetsActions from '../../actions/filterSets';
-import * as FiltersActions from '../../actions/filters';
-import * as ObjectsActions from '../../actions/objects';
-import { BREAKPOINTS } from '../../constants';
+import { selectFilterSet } from '../../actions/filterSets';
 
-class CollectionFiltersMenuItem extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
+const CollectionFiltersMenuItem = ({ selectFilterSet, slug, svgId, title, tooltip, visibleFilterSet }) => {
+  let filterClassNames = 'btn-collection-filter font-zeta color-light';
+  if (slug === 'search') filterClassNames = `${filterClassNames} btn-collection-filter--search`;
+  if (slug === 'shuffle') filterClassNames = `${filterClassNames} btn-collection-filter--shuffle`;
+  if (slug === visibleFilterSet) filterClassNames = `${filterClassNames} is-selected`;
 
-  handleClick() {
-    this.props.selectFilterSet(this.props.slug);
-  }
-
-  getClassNames() {
-    const slug = this.props.slug;
-    let classNames = 'btn-collection-filter font-zeta color-light';
-
-    if (slug === 'search') {
-      classNames += ' btn-collection-filter--search';
-    } else if (slug === 'shuffle') {
-      classNames += ' btn-collection-filter--shuffle';
-    }
-
-    if (slug === this.props.filterSets.visibleFilterSet) {
-      classNames += ' is-selected';
-    }
-
-    return classNames;
-  }
-
-  render() {
-    return (
-      <button
-        className={this.getClassNames()}
-        onClick={this.handleClick}
-        data-tip={this.props.tooltip}
-        data-for="collectionFilterMenuItem"
-      >
-        <div className="button-inner">
-          <div className='button-inner__content'>
-            <Icon svgId={this.props.svgId} classes='collection-filter-icon' />
-            <MediaQuery minWidth={BREAKPOINTS.mobile_max}>
-              <span>
-                {this.props.title}
-              </span>
-            </MediaQuery>
-          </div>
+  return (
+    <button
+      className={filterClassNames}
+      onClick={() => selectFilterSet(slug)}
+      data-tip={tooltip}
+      data-for='collectionFilterMenuItem'
+    >
+      <div className='button-inner'>
+        <div className='button-inner__content'>
+          <Icon svgId={svgId} classes='collection-filter-icon' />
+            <span className='button-inner__text'>{title}</span>
         </div>
-      </button>
-    );
-  }
+      </div>
+    </button>
+  );
 }
 
-const mapStateToProps = state => {
-  return {
-    filterSets: state.filterSets
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators(Object.assign(
-    {},
-    FilterSetsActions,
-    FiltersActions,
-    ObjectsActions
-  ), dispatch);
-}
+const mapStateToProps = (state) => ({ visibleFilterSet: state.filterSets.visibleFilterSet });
+const mapDispatchToProps = (dispatch) => (bindActionCreators(Object.assign({}, { selectFilterSet }), dispatch));
 
 export default connect(mapStateToProps, mapDispatchToProps)(CollectionFiltersMenuItem);
