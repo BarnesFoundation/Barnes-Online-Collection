d2ed2182c8b8613c02ecbdc9d32141d24ddacefc
agallagher777 <agallagher@barnesfoundation.org>
2020-02-19 12:45:14 -0500

    Adding general search filter to condition that will add --applied modifier in CollectionFiltersMenuItem.

--- src/components/CollectionFilters/CollectionFiltersMenuItem.jsx
+++ src/components/CollectionFilters/CollectionFiltersMenuItem.jsx
@@ -4,13 +4,13 @@ import { connect } from 'react-redux';
 import Icon from '../Icon';
 import { selectFilterSet } from '../../actions/filterSets';
 
-const CollectionFiltersMenuItem = ({ selectFilterSet, slug, svgId, title, visibleFilterSet, hasAdvancedFilters }) => {
+const CollectionFiltersMenuItem = ({ selectFilterSet, slug, svgId, title, visibleFilterSet, hasAdvancedFiltersOrSearch }) => {
   let filterClassNames = 'btn-collection-filter font-zeta color-light';
   if (slug === 'search') {
     filterClassNames = `${filterClassNames} btn-collection-filter--search`;
     
     // Check if there are any advanced filters, if so, add class for notification.
-    if (hasAdvancedFilters) filterClassNames = `${filterClassNames} btn-collection-filter--applied`
+    if (hasAdvancedFiltersOrSearch) filterClassNames = `${filterClassNames} btn-collection-filter--applied`
   }
   if (slug === 'shuffle') filterClassNames = `${filterClassNames} btn-collection-filter--shuffle`;
   if (slug === visibleFilterSet) filterClassNames = `${filterClassNames} is-selected`;
@@ -33,10 +33,16 @@ const CollectionFiltersMenuItem = ({ selectFilterSet, slug, svgId, title, visibl
 const mapStateToProps = (state) => ({
   visibleFilterSet: state.filterSets.visibleFilterSet,
 
-  // Check if there are any advanced filters.
-  hasAdvancedFilters: Object.values(state.filters.advancedFilters)
+  // Check if there are any advanced filters or search.
+  hasAdvancedFiltersOrSearch: (
+    // Check advanced filters
+    Object.values(state.filters.advancedFilters)
     .flatMap(Object.keys) // Get all keys from advancedFilter's children.
-    .some(obj => obj.length) // Check if there are any keys.
+    .some(obj => obj.length) || // Check if there are any keys.
+
+    // Check search.
+    state.filters.search
+  )
 });
 const mapDispatchToProps = dispatch => (bindActionCreators(Object.assign({}, { selectFilterSet }), dispatch));
 
