355c41f40bdb881fc4c1861e5f6fd83bb88621df
agallagher777 <agallagher@barnesfoundation.org>
2020-02-19 10:20:21 -0500

    Adjusting hero styling.

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -20,40 +20,43 @@ import './landingPage.css';
 /**
  * Header JSX for landing page.
  */
-const LandingPageHeader = () => (
-  <div className='o-hero o-hero--landing-page'>
-    <div className='o-hero__inner'>
-      <div className='container o-hero__container'>
-        <div className='o-hero__copy'>
-          <p className='o-hero__supporting'>The Barnes Foundation houses the world's largest collections of Renoir and Cezanne, and important works by Matisse, Picasso and Modigliani.</p>
+class LandingPageHeader extends Component {
+  constructor(props) {
+    super(props);
+
+    this.ref = null; // For getting height.
+  }
+
+  render() {
+
+    return (
+      <div
+        className='o-hero o-hero--landing-page'
+        style={{ height: this.ref ? this.ref.offsetHeight : 'auto' }}
+      >
+        <div className='o-hero__inner'>
+          <div className='container o-hero__container'>
+            <div className='o-hero__copy'>
+              <p className='o-hero__supporting'>The Barnes Foundation houses the world's largest collections of Renoir and Cezanne, and important works by Matisse, Picasso and Modigliani.</p>
+            </div>
+          </div>
+        </div>
+        <div className='o-hero__video-wrapper'>
+          <video
+            ref={ref => this.ref = ref}
+            style={{ opacity: 1 }}
+            className='o-hero__video'
+            src={heroVideo}
+            autoPlay
+            loop
+            muted={true}
+            playsInline
+          />
         </div>
       </div>
-    </div>
-    <div className='o-hero__video-wrapper'>
-      <video
-        style={{ opacity: 1 }}
-        className='o-hero__video'
-        src={heroVideo}
-        autoPlay
-        loop
-        muted={true}
-        playsInline
-      />
-    </div>
-    {/* <picture>
-      <source
-        media='(min-width: 990px)'
-        srcSet='https://barnesfoundation.imgix.net/sharedBackgroundImages/Personalized-Docent-Tour_190122_145459.jpg?crop=faces&amp;fit=crop&amp;fm=pjpg&amp;fp-x=0.5&amp;fp-y=0.5&amp;h=800&amp;ixlib=php-2.1.1&amp;w=1380' type='image/jpeg' />
-      <source
-        media='(min-width: 650px)'
-        srcSet='https://barnesfoundation.imgix.net/sharedBackgroundImages/Personalized-Docent-Tour_190122_145459.jpg?crop=faces&amp;fit=crop&amp;fm=pjpg&amp;fp-x=0.5&amp;fp-y=0.5&amp;h=574&amp;ixlib=php-2.1.1&amp;w=880' type='image/jpeg' />
-      <img
-        className='o-hero__image'
-        src='https://barnesfoundation.imgix.net/sharedBackgroundImages/Personalized-Docent-Tour_190122_145459.jpg?crop=faces&amp;fit=crop&amp;fm=pjpg&amp;fp-x=0.5&amp;fp-y=0.5&amp;h=380&amp;ixlib=php-2.1.1&amp;w=380'
-        alt='' />
-    </picture> */}
-  </div>
-);
+    )
+  }
+}
 
 class LandingPage extends Component {
   constructor(props) {
