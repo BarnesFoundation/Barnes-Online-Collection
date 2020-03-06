ddea35a5ef658339dbe9b627e2bda44c9c6d51aa
agalla777 <agallagher@barnesfoundation.org>
2020-03-06 16:50:14 -0500

    Fixing ref to forceupdate and keeping value of hidden input static for share copy link.

--- src/components/ShareDialog/ShareDialog.jsx
+++ src/components/ShareDialog/ShareDialog.jsx
@@ -30,16 +30,25 @@ class Share extends React.Component {
 		const shareLink = createShareForPlatform(people, title, id, platform, this.props.object.imageUrlLarge);
 
 		if (platform === sharePlatforms.COPY_URL) {
-
-			this.setState({ copyText: shareLink });
 			this.copy.select();
 			document.execCommand('copy');
 		}
 		else window.open(shareLink, '_blank');
 	};
 
+	/**
+	 * Set ref and force update, as this will not set ref until after first render.
+ 	 * @param {HTMLElement} ref - element to set this.ref to.
+	 */
+	setRef = (ref) => {
+		this.copy = ref;
+
+		this.forceUpdate();
+	}
+
 	render() {
-		const { copyText, showShareDialog } = this.state;
+		const { showShareDialog } = this.state;
+		const { id, people, title } = this.props.object;
 
 		return (
 			<div className='panel-button panel-button--share' onClick={() => { this.toggleShareDialog(); }}>
@@ -50,7 +59,11 @@ class Share extends React.Component {
 						<a className="share-dialog__link" onClick={() => { this.onShareLinkClick(sharePlatforms.PINTEREST) }}>Pinterest</a>
 						<a className="share-dialog__link" onClick={() => { this.onShareLinkClick(sharePlatforms.EMAIL) }}>Email</a>
 						<a className="share-dialog__link" onClick={() => { this.onShareLinkClick(sharePlatforms.COPY_URL) }}>Copy Link</a>
-						<input style={{ position: 'absolute', height: 0, opacity: '.01' }} ref={(area) => this.copy = area} value={copyText} />
+						<input
+							style={{ position: 'absolute', height: 0, opacity: '.01' }}
+							ref={this.setRef}
+							value={createShareForPlatform(people, title, id, sharePlatforms.COPY_URL, this.props.object.imageUrlLarge)}
+						/>
 					</div>
 				}
 				<div className='panel-button__content'>
