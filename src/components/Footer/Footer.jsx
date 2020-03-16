3703f1f1935e30b32c438d7a7319ddb0a95e03fb
agalla777 <agallagher@barnesfoundation.org>
2020-03-16 10:29:10 -0400

    Reordering imports in footer.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import MediaQuery from 'react-responsive';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import Icon from '../Icon';
 import { MoreFromCollection } from '../../components/Footer/MoreFromCollection'
 import { MAIN_WEBSITE_DOMAIN, BREAKPOINTS } from '../../constants';
 import './footer.css';
-import axios from 'axios';
+
 
 const NEWSLETTER_SESSION_STORAGE_KEY = 'isNewsletterSubscribed';
 const EMAIL_REGEX = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
