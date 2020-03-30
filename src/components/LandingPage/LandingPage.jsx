1dde130cc271edd3dc67344ea087bd1b51283faf
agalla777 <agallagher@barnesfoundation.org>
2020-03-30 12:06:02 -0400

    Removing unnecessary ref.

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -24,8 +24,6 @@ class LandingPageHeader extends Component {
   constructor(props) {
     super(props);
 
-    this.ref = null; // For getting height.
-
     this.sto = null;
     this.si = null;
     this.textSto = null;
