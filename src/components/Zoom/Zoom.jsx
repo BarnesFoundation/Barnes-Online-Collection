47e67160cfb534a04f28854d5083b20523bbc5c5
agalla777 <agallagher@barnesfoundation.org>
2020-04-29 09:48:47 -0400

    Adding OSD.

--- src/components/Zoom/Zoom.jsx
+++ src/components/Zoom/Zoom.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import OpenSeadragon from 'openseadragon';
 import leaflet from 'leaflet';
 import 'leaflet-iiif';
 import 'leaflet.fullscreen';
@@ -26,8 +27,15 @@ class Zoom extends Component {
     const { catchFailureInViewer } = this.props;
 
     try {
-      await axios.get(url);
+      const { data: res } = await axios.get(url);
+      const root = res['@id'].replace('http://localhost:8080/', '');
+      res['@id'] = `${IMAGE_BASE_URL}/tiles/${root}`;
+
+      console.log(root);
+      
+      return res;
     } catch (e) {
+      console.log(e);
       if (catchFailureInViewer) {
         catchFailureInViewer();
       }
@@ -39,62 +47,108 @@ class Zoom extends Component {
    */
   componentDidUpdate(prevProps) {
     if (prevProps.id !== this.props.id) {
-      this.map.off();
-      this.map.remove();
+      // this.map.off();
+      // this.map.remove();
+
+      // this.osd.destroy();
 
       this.mountLeaflet();
     }
   }
 
-  mountLeaflet = () => {
+  mountLeaflet = async () => {
     const { id } = this.props;
+    const res = await this.checkURL(`${IMAGE_BASE_URL}/tiles/${id}/info.json`);
 
     if (this.ref) {
-      this.map = leaflet.map(this.ref, {
-        center: [0, 0],
-        crs: leaflet.CRS.Simple,
-        zoom: 1,
-        fullscreenControl: true,
-        fullscreenControlOptions: {
-          position: 'topleft'
-        },
+      console.log('https://collection-tif-tiler.s3.amazonaws.com/tiles/BF1008/info.json');
+      console.log(`${IMAGE_BASE_URL}/tiles/${id}/info.json`);
+      this.osd = OpenSeadragon({
+        element: this.ref,
+        visibilityRatio: 1.0,
+        constrainDuringPan: true,
+        
+        // tileSources: [`${IMAGE_BASE_URL}/tiles/${id}/info.json`],
+        // tileSources: ['https://collection-tif-tiler.s3.amazonaws.com/tiles/BF25/info.json'],
+        tileSources: [res],
+        
+        navigatorBackground: '#000',
+        showNavigationControl: false,
+        immediateRender: true,
+        minZoomLevel: 4,
       });
 
-      const url = `${IMAGE_BASE_URL}/tiles/${id}/info.json`;
-
-      const iiifLayer = leaflet.tileLayer.iiif(url, {
-        quality: 'color',
-        tileFormat: 'jpg',
-        fitBounds: true,
-        setMaxBounds: true,
+      this.osd.addHandler('open', () => {
+        // const imageBounds = this.osd.world.getItemAt(0).getBounds();
+        // this.osd.viewport.fitBounds(imageBounds, true);
       });
-
-
-      // Add event handler to increase zoom if tile is not found.
-      iiifLayer.on('tileerror', () => {
-        const currentZoom = this.map.getZoom(); // Calculate current zoom.
-
-        // If missing a layer, zoom in and set min zoom to next level, preventing zoom out.
-        if (currentZoom !== this.map.getMaxZoom()) {
-          this.map.setMinZoom(currentZoom + 1);
-          this.map.setZoom(currentZoom + 1);
-        }
-      });
-
-      // Set up scroll wheel when full screen.
-      this.map.on('enterFullscreen', () => this.map.scrollWheelZoom.enable());
-      this.map.on('exitFullscreen', () => this.map.scrollWheelZoom.disable());
-
-      this.map.addLayer(iiifLayer);
-      this.map.scrollWheelZoom.disable();
-
-      this.checkURL(url);
     }
   }
+
+  // mountLeaflet = () => {
+  //   const { id } = this.props;
+
+  //   if (this.ref) {
+  //     this.map = leaflet.map(this.ref, {
+  //       center: [0, 0],
+  //       crs: leaflet.CRS.Simple,
+  //       zoom: 1,
+  //       fullscreenControl: true,
+  //       fullscreenControlOptions: {
+  //         position: 'topleft'
+  //       },
+  //     });
+
+  //     const url = `${IMAGE_BASE_URL}/tiles/${id}/info.json`;
+  //     // const url = 'https://collection-tif-tiler.s3.amazonaws.com/tiles/BF25/info.json';
+
+  //     const iiifLayer = leaflet.tileLayer.iiif(url, {
+  //       quality: 'color',
+  //       tileFormat: 'jpg',
+  //       fitBounds: true,
+  //       setMaxBounds: true,
+  //     });
+
+
+  //     // Add event handler to increase zoom if tile is not found.
+  //     iiifLayer.on('tileerror', () => {
+  //       const currentZoom = this.map.getZoom(); // Calculate current zoom.
+
+  //       // If missing a layer, zoom in and set min zoom to next level, preventing zoom out.
+  //       if (currentZoom !== this.map.getMaxZoom()) {
+  //         this.map.invalidateSize();
+  //         // this.map.setMinZoom(currentZoom + 1);
+  //         this.map.setZoom(currentZoom + 1);
+  //       }
+  //     });
+
+  //     // Set up scroll wheel when full screen.
+  //     this.map.on('enterFullscreen', () => this.map.scrollWheelZoom.enable());
+  //     this.map.on('exitFullscreen', () => this.map.scrollWheelZoom.disable());
+
+  //     this.map.addLayer(iiifLayer);
+  //     this.map.scrollWheelZoom.disable();
+
+  //     this.checkURL(url);
+  //   }
+  // }
 
   render() {
     return (
         <section className="zoom">
+          {/* <div
+          style={{
+            height: '1000px',
+            width: '500px',
+          }}
+          ref={ref => {
+            if (!this.ref) {
+              this.ref = ref;
+              this.mountLeaflet();
+            }
+          }}>
+
+          </div> */}
           <div className="map-container">
             <div ref={(ref) => {
               if (!this.ref) {
