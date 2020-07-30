c913e5da7dad807e293b8dca923626bce46a1e2d
agalla777 <agallagher@barnesfoundation.org>
2020-07-30 13:12:39 -0400

    Adjusting port to match package json.

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4002;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
