7c9d39af2edcb0f7fb74240fa1831a1ea9781c44
Leigh Passamano <leighayna.josele@gmail.com>
2021-02-25 13:37:21 -0500

    Refactor sticky list component to separate out the list sections

--- src/components/StickyList/StickyList.jsx
+++ src/components/StickyList/StickyList.jsx
@@ -1,6 +1,28 @@
-import React, { Component } from 'react';
-import $ from 'jquery';
-import './stickyList.css'
+import React, { Component } from "react";
+import $ from "jquery";
+import "./stickyList.css";
+
+class StickyListSection extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.match.params !== nextProps.match.params) {
+    }
+  }
+
+  render() {
+    const { header, content } = this.props;
+
+    return (
+      <div className="sticky-list__section">
+        <div className="sticky-list__section__header">{header}</div>
+        <div className="sticky-list__section__content">{content}</div>
+      </div>
+    );
+  }
+}
 
 export default class StickyList extends Component {
   constructor(props) {
@@ -13,116 +35,99 @@ export default class StickyList extends Component {
   }
 
   componentDidMount() {
-    var stickyHeaders = (function() {
-
+    var stickyHeaders = (function () {
       var $window = $(window),
-          $stickies;
-    
-      var load = function(stickies) {
-    
+        $stickies;
+
+      var load = function (stickies) {
         if (typeof stickies === "object" && stickies.length > 0) {
-          $stickies = stickies.each(function() {
-    
-            var $thisSticky = $(this).wrap('<div class="sticky-list__section__header-wrap" />');
-      
+          $stickies = stickies.each(function () {
+            var $thisSticky = $(this).wrap(
+              '<div class="sticky-list__section__header-wrap" />'
+            );
+
             $thisSticky
-                .data('originalPosition', $thisSticky.offset().top)
-                .data('originalHeight', $thisSticky.outerHeight())
-                  .parent()
-                  .height($thisSticky.outerHeight()); 			  
+              .data("originalPosition", $thisSticky.offset().top)
+              .data("originalHeight", $thisSticky.outerHeight())
+              .parent()
+              .height($thisSticky.outerHeight());
           });
-    
-          $window.off("scroll.stickies").on("scroll.stickies", function() {
-          _whenScrolling();		
+
+          $window.off("scroll.stickies").on("scroll.stickies", function () {
+            _whenScrolling();
           });
         }
       };
-    
-      var _whenScrolling = function() {
-    
-        $stickies.each(function(i) {			
-    
-          var $thisSticky = $(this),
-              $stickyPosition = $thisSticky.data('originalPosition'),
-              $stickyHeight = $thisSticky.data('originalHeight'),
-              $stickyParentHeight = $thisSticky.parents('.sticky-list__section').outerHeight();
 
-          if ($stickyPosition <= $window.scrollTop()) { //  Handle scrolling down the page
+      var _whenScrolling = function () {
+        $stickies.each(function (i) {
+          var $thisSticky = $(this),
+            $stickyPosition = $thisSticky.data("originalPosition"),
+            $stickyHeight = $thisSticky.data("originalHeight"),
+            $stickyParentHeight = $thisSticky
+              .parents(".sticky-list__section")
+              .outerHeight();
+
+          if ($stickyPosition <= $window.scrollTop()) {
+            //  Handle scrolling down the page
             // when original position of sticky moves above the window, make sticky fixed position
             var $nextSticky = $stickies.eq(i + 1),
-                $nextStickyPosition = $nextSticky.data('originalPosition') - $stickyHeight;
-            
+              $nextStickyPosition =
+                $nextSticky.data("originalPosition") - $stickyHeight;
+
             $thisSticky.addClass("fixed");
 
-            if (($stickyPosition + $stickyParentHeight) <= $window.scrollTop()) {
+            if ($stickyPosition + $stickyParentHeight <= $window.scrollTop()) {
               // when bottom of sticky section moves above the window, make sticky absolute position above the next sticky
               $thisSticky.addClass("absolute").css("top", $nextStickyPosition);
             } else {
               // when the bottom of the sticky section moves into the window, remove absolute position
               // this prevents the last sticky in a list from remaining in the window
-              $thisSticky.removeClass("absolute")
+              $thisSticky.removeClass("absolute");
             }
-          } else { // Handle scrolling up the page
+          } else {
+            // Handle scrolling up the page
             // when original position of sticky moves into the window, remove fixed position
             var $prevSticky = $stickies.eq(i - 1);
-    
+
             $thisSticky.removeClass("fixed");
-    
-            if ($prevSticky.length > 0 && $window.scrollTop() <= $stickyPosition - $stickyHeight) {
+
+            if (
+              $prevSticky.length > 0 &&
+              $window.scrollTop() <= $stickyPosition - $stickyHeight
+            ) {
               // when bottom of sticky moves into window, remove absolute position
-              console.log('removing absolute!')
+              console.log("removing absolute!");
               $prevSticky.removeClass("absolute").removeAttr("style");
             }
           }
         });
       };
-    
+
       return {
-        load: load
+        load: load,
       };
     })();
-    
-    $(function() {
+
+    $(function () {
       stickyHeaders.load($(".sticky-list__section__header"));
     });
   }
 
   render() {
-    const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc vel risus commodo viverra maecenas accumsan lacus vel. Semper auctor neque vitae tempus quam. Accumsan tortor posuere ac ut consequat semper. Volutpat ac tincidunt vitae semper quis lectus nulla at volutpat. Tortor dignissim convallis aenean et tortor at. Integer enim neque volutpat ac tincidunt vitae semper quis lectus. Tempor id eu nisl nunc. Vel facilisis volutpat est velit egestas dui id ornare arcu. Libero volutpat sed cras ornare.'
-
+    const { heroImage, title, sections } = this.props;
     return (
       <div className="sticky-list">
         <div className="sticky-list__hero">
-          <img
-            className="sticky-list__hero__image"
-            src={this.props.heroImage}
+          <img className="sticky-list__hero__image" src={heroImage} />
+          <h2 className="sticky-list__hero__title">{title}</h2>
+        </div>
+        {sections.map((section) => (
+          <StickyListSection
+            header={section.header}
+            content={section.content}
           />
-          <h2 className="sticky-list__hero__title">{this.props.title}</h2>
-        </div>
-        <div className="sticky-list__section">
-          <div className="sticky-list__section__header">Room 1</div>
-          <div className="sticky-list__section__content">{lorem}</div>
-        </div>
-        <div className="sticky-list__section">
-          <div className="sticky-list__section__header">Room 2</div>
-          <div className="sticky-list__section__content">{lorem}</div>
-        </div>
-        <div className="sticky-list__section">
-          <div className="sticky-list__section__header">Room 3</div>
-          <div className="sticky-list__section__content">{lorem}</div>
-        </div>
-        <div className="sticky-list__section">
-          <div className="sticky-list__section__header">Room 4</div>
-          <div className="sticky-list__section__content">{lorem}</div>
-        </div>
-        <div className="sticky-list__section">
-          <div className="sticky-list__section__header">Room 5</div>
-          <div className="sticky-list__section__content">{lorem}</div>
-        </div>
-        <div className="sticky-list__section">
-          <div className="sticky-list__section__header">Room 6</div>
-          <div className="sticky-list__section__content">{lorem}</div>
-        </div>
+        ))}
       </div>
     );
   }
--- src/components/TourPage/TourPage.jsx
+++ src/components/TourPage/TourPage.jsx
@@ -1,85 +1,106 @@
-import React from 'react';
-import axios from 'axios';
-import { withRouter } from 'react-router'
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as ObjectActions from '../../actions/object';
-import { SiteHeader } from '../../components/SiteHeader/SiteHeader';
-import SiteHtmlHelmetHead from '../SiteHtmlHelmetHead';
-import HtmlClassManager from '../HtmlClassManager';
-import Footer from '../Footer/Footer';
-import StickyList from '../StickyList/StickyList';
-import './tourPage.css'
+import React from "react";
+import axios from "axios";
+import { withRouter } from "react-router";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as ObjectActions from "../../actions/object";
+import { SiteHeader } from "../../components/SiteHeader/SiteHeader";
+import SiteHtmlHelmetHead from "../SiteHtmlHelmetHead";
+import HtmlClassManager from "../HtmlClassManager";
+import Footer from "../Footer/Footer";
+import StickyList from "../StickyList/StickyList";
+import "./tourPage.css";
+
+// to do
+// sticky-list can't be fixed because then a scroll event does not trigger
+// and that means that the top menu header does not collapse
+// look into the scroll listener that is done in the SiteHeader and see if we
+// can adapt to the section headers
 
 class TourPage extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			tourId: null,
-			title: null,
+    this.state = {
+      tourId: null,
+      title: null,
       objects: null,
       roomOrder: null,
-		};
-	};
+    };
+  }
 
-	async componentDidMount() {
+  async componentDidMount() {
+    // Extract the slug for this tour
+    const { id } = this.props.match.params;
 
-		// Extract the slug for this tour
-		const { id } = this.props.match.params;
-
-		// If we have a slug, retrieve information for the tour
-		if (id) {
-			try {
-				const tourResponse = await axios.get(`/api/tour/${id}`);
-				this.setState({
-					title: tourResponse.data.title,
+    // If we have a slug, retrieve information for the tour
+    if (id) {
+      try {
+        const tourResponse = await axios.get(`/api/tour/${id}`);
+        this.setState({
+          title: tourResponse.data.title,
           objects: tourResponse.data.data.hits.hits,
-          roomOrder: tourResponse.data.customRoomOrder
-				});
-			}
+          roomOrder: tourResponse.data.customRoomOrder,
+        });
+      } catch (error) {
+        console.log(
+          `An error occurred retrieving the tour for id ${id}`,
+          error
+        );
+      } finally {
+        this.setState({
+          ...this.state,
+          tourId: id,
+        });
+      }
+    }
+  }
 
-			catch (error) {
-				console.log(`An error occurred retrieving the tour for id ${id}`, error);
-			}
+  formatTourData() {
+    const lorem =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc vel risus commodo viverra maecenas accumsan lacus vel. Semper auctor neque vitae tempus quam. Accumsan tortor posuere ac ut consequat semper. Volutpat ac tincidunt vitae semper quis lectus nulla at volutpat. Tortor dignissim convallis aenean et tortor at. Integer enim neque volutpat ac tincidunt vitae semper quis lectus. Tempor id eu nisl nunc. Vel facilisis volutpat est velit egestas dui id ornare arcu. Libero volutpat sed cras ornare.";
 
-			finally {
-				this.setState({
-					...this.state,
-					tourId: id
-				})
-			}
-		}
-	};
+    return [
+      { header: "Room 1", content: lorem },
+      { header: "Room 2", content: lorem },
+      { header: "Room 3", content: lorem },
+      { header: "Room 4", content: lorem },
+      { header: "Room 5", content: lorem },
+      { header: "Room 6", content: lorem },
+      { header: "Room 7", content: lorem },
+    ];
+  }
 
-	render() {
-		const { tourId, title, objects } = this.state;
+  render() {
+    const { tourId, title, objects } = this.state;
 
-    const header = <div className="tour-page-hero"><img src='https://d2r83x5xt28klo.cloudfront.net/6814_mpfCoboPefnN6Ws6_n.jpg' style={{"width": "100%", "margin-top": "-150px"}} /><h2 className="tour-title">{title}</h2></div>
-
-		return (
-			<div className="app app-tour-page">
-				<SiteHtmlHelmetHead />
-				<HtmlClassManager />
-				<SiteHeader isTour />
-				{(tourId && title && objects)
-				? // Display the tour if it was located
-					<div className="tour-page-container">
-					<StickyList title={title} />
-					</div>
-				: // Otherwise, no tour found for that id
-					<div className="container tour-page-container">
-					<p>Could not find tour with id "{tourId}"</p>
-					</div>
-				}
-				<div style={{'background-color': 'white', 'z-index': '1'}}>
-					<Footer />
-				</div>
-			</div>
-
-		);
-	};
-};
+	return (
+      <div className="app app-tour-page">
+        <SiteHtmlHelmetHead />
+        <HtmlClassManager />
+        <SiteHeader isTour />
+        {tourId && title && objects ? (
+          // Display the tour if it was located
+          <div className="tour-page-container">
+            <StickyList
+              title={title}
+              heroImage="https://d2r83x5xt28klo.cloudfront.net/6814_mpfCoboPefnN6Ws6_n.jpg"
+              sections={this.formatTourData()}
+            />
+          </div>
+        ) : (
+          // Otherwise, no tour found for that id
+          <div className="container tour-page-container">
+            <p>Could not find tour with id "{tourId}"</p>
+          </div>
+        )}
+        <div style={{ "background-color": "white", "z-index": "1" }}>
+          <Footer />
+        </div>
+      </div>
+    );
+  }
+}
 
 function mapStateToProps(state) {
   return {
@@ -88,11 +109,10 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators(Object.assign(
-    {},
-    ObjectActions,
-  ), dispatch);
+  return bindActionCreators(Object.assign({}, ObjectActions), dispatch);
 }
 
 const compWithRouter = withRouter(TourPage);
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(compWithRouter));
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(compWithRouter)
+);
