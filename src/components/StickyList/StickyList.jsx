643bef80d3bf598e4f8ebd4c780f0ccccd291f3d
Leigh Passamano <leighayna.josele@gmail.com>
2021-03-01 11:47:28 -0500

    Refactor sticky headers to use ref

--- src/components/StickyList/StickyList.jsx
+++ src/components/StickyList/StickyList.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import classnames from "classnames";
-import $ from "jquery";
 import { getObjectMetaDataHtml } from "../ArtObjectPageComponents/PanelVisuallyRelated";
 import { formatTourData } from "../TourPage/tourPageHelper";
 import { parseObject } from "../../objectDataUtils";
@@ -86,12 +85,49 @@ class ObjectCard extends Component {
 class StickyListSection extends Component {
   constructor(props) {
     super(props);
+
+    this.handleScroll = this.handleScroll.bind(this);
+    this.state = { isFixed: false, isAbsolute: true };
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll(event) {
+    let isFixed = this.state.isFixed;
+    const sectionBounds = this.section.getBoundingClientRect();
+
+    if (sectionBounds.top <= 0 && sectionBounds.bottom > 0) {
+      // when the section is visible, header is fixed at window top
+      isFixed = true;
+    } else {
+      // when the section is not visible, header is absolutely positioned at section top
+      isFixed = false;
+    }
+
+    this.setState({ isFixed: isFixed, isAbsolute: !isFixed });
   }
 
   render() {
     return (
-      <div className="sticky-list__section">
-        <div className="sticky-list__section__header">{this.props.header}</div>
+      <div
+        className="sticky-list__section"
+        ref={(section) => {
+          this.section = section;
+        }}
+      >
+        <div
+          className={classnames("sticky-list__section__header", {
+            fixed: this.state.isFixed,
+            absolute: this.state.isAbsolute,
+          })}
+          ref={(header) => {
+            this.header = header;
+          }}
+        >
+          {this.props.header}
+        </div>
         <div className="sticky-list__section__content">
           {this.props.section.content.map((obj) => {
             return <ObjectCard object={parseObject(obj)} key={obj.id} />;
@@ -106,85 +142,6 @@ export default class StickyList extends Component {
   constructor(props) {
     super(props);
     this.getHeroImage = this.getHeroImage.bind(this);
-  }
-
-  componentDidMount() {
-    var stickyHeaders = (function () {
-      var $window = $(window),
-        $stickies;
-
-      var load = function (stickies) {
-        if (typeof stickies === "object" && stickies.length > 0) {
-          $stickies = stickies.each(function () {
-            var $thisSticky = $(this).wrap(
-              '<div class="sticky-list__section__header-wrap" />'
-            );
-
-            $thisSticky
-              .data("originalPosition", $thisSticky.offset().top)
-              .data("originalHeight", $thisSticky.outerHeight())
-              .parent()
-              .height($thisSticky.outerHeight());
-          });
-
-          $window.off("scroll.stickies").on("scroll.stickies", function () {
-            _whenScrolling();
-          });
-        }
-      };
-
-      var _whenScrolling = function () {
-        $stickies.each(function (i) {
-          var $thisSticky = $(this),
-            $stickyPosition = $thisSticky.data("originalPosition"),
-            $stickyHeight = $thisSticky.data("originalHeight"),
-            $stickyParentHeight = $thisSticky
-              .parents(".sticky-list__section")
-              .outerHeight();
-
-          if ($stickyPosition <= $window.scrollTop()) {
-            //  Handle scrolling down the page
-            // when original position of sticky moves above the window, make sticky fixed position
-            var $nextSticky = $stickies.eq(i + 1),
-              $nextStickyPosition =
-                $nextSticky.data("originalPosition") - $stickyHeight;
-
-            $thisSticky.addClass("fixed");
-
-            if ($stickyPosition + $stickyParentHeight <= $window.scrollTop()) {
-              // when bottom of sticky section moves above the window, make sticky absolute position above the next sticky
-              $thisSticky.addClass("absolute").css("top", $nextStickyPosition);
-            } else {
-              // when the bottom of the sticky section moves into the window, remove absolute position
-              // this prevents the last sticky in a list from remaining in the window
-              $thisSticky.removeClass("absolute");
-            }
-          } else {
-            // Handle scrolling up the page
-            // when original position of sticky moves into the window, remove fixed position
-            var $prevSticky = $stickies.eq(i - 1);
-
-            $thisSticky.removeClass("fixed");
-
-            if (
-              $prevSticky.length > 0 &&
-              $window.scrollTop() <= $stickyPosition - $stickyHeight
-            ) {
-              // when bottom of sticky moves into window, remove absolute position
-              $prevSticky.removeClass("absolute").removeAttr("style");
-            }
-          }
-        });
-      };
-
-      return {
-        load: load,
-      };
-    })();
-
-    $(function () {
-      stickyHeaders.load($(".sticky-list__section__header"));
-    });
   }
 
   getHeroImage() {
