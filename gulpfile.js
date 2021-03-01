d549dbd9ee95767f8360180bb85c495a30213b14
Christopher Jativa <cjativa@barnesfoundation.org>
2021-02-28 21:56:44 -0500

    include the tours.json file, remove breaking node engine setting

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,25 @@ const { src, dest, series } = require('gulp');
 const zip = require('gulp-zip');
 
 function build() {
-    return src(['package.json', '.npmrc', 'build/**', 'server/**', 'scripts/**', 'src/artObjectTitles.json', 'src/ensembleIndexes.js', '.ebextensions/**'], {base: './'})
-    .pipe(dest('dist'));
+	return src(['package.json',
+		'.npmrc',
+		'build/**',
+		'server/**',
+		'server/constants/tours.json',
+		'scripts/**',
+		'src/artObjectTitles.json',
+		'src/ensembleIndexes.js',
+		'.ebextensions/**'], { base: './' })
+		.pipe(dest('dist'));
 }
 
 function zipDist() {
-    return src('./dist/**', { dot: true })
-        .pipe(zip('dist.zip'))
-        .pipe(dest('./'));
+	return src('./dist/**', { dot: true })
+		.pipe(zip('dist.zip'))
+		.pipe(dest('./'));
 }
 
 exports.default = series(
-    build,
-    zipDist
+	build,
+	zipDist
 )
