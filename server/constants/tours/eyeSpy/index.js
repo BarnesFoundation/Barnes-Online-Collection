87b03e2e15115866e0fc485472ec49184e247b0c
livsteinmetz <olivia.r.steinmetz@gmail.com>
2022-02-27 20:52:35 -0500

    add tourId to index.js

--- server/constants/tours/eyeSpy/index.js
+++ server/constants/tours/eyeSpy/index.js
@@ -1,11 +1,13 @@
 const test = require("./test.json");
 const dec2021 = require("./dec-2021.json");
 const feb2022 = require("./feb-2022.json");
+const mar2022 = require("./mar-2022.json");
 
 // Key is the slug/tourId of the tour
 module.exports = {
     "dec-2021": dec2021,
     "feb-2022": feb2022, 
+    "mar-2022": mar2022, 
     // Tour for test suite
     test,
 }
