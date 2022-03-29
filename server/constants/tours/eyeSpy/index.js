f7364ab40d30e4f1aa84c25fb63d7bcff7c3f524
livsteinmetz <olivia.r.steinmetz@gmail.com>
2022-03-29 05:01:40 -0400

    import april eyespy to index.js

--- server/constants/tours/eyeSpy/index.js
+++ server/constants/tours/eyeSpy/index.js
@@ -2,12 +2,14 @@ const test = require("./test.json");
 const dec2021 = require("./dec-2021.json");
 const feb2022 = require("./feb-2022.json");
 const mar2022 = require("./mar-2022.json");
+const apr2022 = require9("./apr-2022.json");
 
 // Key is the slug/tourId of the tour
 module.exports = {
     "dec-2021": dec2021,
     "feb-2022": feb2022, 
     "mar-2022": mar2022, 
+    "apr-2022": apr2022,
     // Tour for test suite
     test,
 }
