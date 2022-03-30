1ec77a7983e5d1b27b8b964343039a0cf0784ca8
Leigh Passamano <leighayna.josele@gmail.com>
2022-03-30 16:31:42 -0400

    Fix typo

--- server/constants/tours/eyeSpy/index.js
+++ server/constants/tours/eyeSpy/index.js
@@ -2,7 +2,7 @@ const test = require("./test.json");
 const dec2021 = require("./dec-2021.json");
 const feb2022 = require("./feb-2022.json");
 const mar2022 = require("./mar-2022.json");
-const apr2022 = require9("./apr-2022.json");
+const apr2022 = require("./apr-2022.json");
 
 // Key is the slug/tourId of the tour
 module.exports = {
