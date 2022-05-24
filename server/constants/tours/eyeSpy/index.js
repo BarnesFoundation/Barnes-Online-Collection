95eaf08b8c48bb0e36ed8a91536063e27fe0d576
livsteinmetz <olivia.r.steinmetz@gmail.com>
2022-05-24 13:30:22 -0400

    add slug/tourid for june

--- server/constants/tours/eyeSpy/index.js
+++ server/constants/tours/eyeSpy/index.js
@@ -4,6 +4,7 @@ const feb2022 = require("./feb-2022.json");
 const mar2022 = require("./mar-2022.json");
 const apr2022 = require("./apr-2022.json");
 const may2022 = require("./may-2022.json");
+const june2022 = require("./june-2022.json");
 
 // Key is the slug/tourId of the tour
 module.exports = {
@@ -12,6 +13,7 @@ module.exports = {
     "mar-2022": mar2022, 
     "apr-2022": apr2022,
     "may-2022": may2022,
+    "june-2022": june2022,
     // Tour for test suite
     test,
 }
