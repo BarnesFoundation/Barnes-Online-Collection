4b8df57258bffc81a4516069bfb95b3cf7012cbf
Chris Jativa <cjativa@barnesfoundation.org>
2023-11-07 08:23:36 -0500

    use config from shared directory

--- src/ensembleIndex.js
+++ src/ensembleIndex.js
@@ -1,7 +1,8 @@
-// todo: deduplicate #imgUrlLogic
-const AWS_BUCKET = process.env.REACT_APP_AWS_BUCKET;
-const IMAGES_PREFIX = process.env.REACT_APP_IMAGES_PREFIX;
-const IMAGE_BASE_URL = process.env.REACT_APP_IMAGE_BASE_URL || `//s3.amazonaws.com/${AWS_BUCKET}`;
+import uiConfig from '../shared/uiConfig';
+
+// TODO - These are used quite a bite throughout codebase. Possibly move to `/shared` directory somewhere
+const IMAGES_PREFIX = uiConfig.imagesPrefix;
+const IMAGE_BASE_URL = uiConfig.imageBaseURL;
 const imageUrlBase = IMAGES_PREFIX ? `${IMAGE_BASE_URL}/${IMAGES_PREFIX}` : IMAGE_BASE_URL;
 const ENSEMBLE = require('./ensembleIndexes');
 
