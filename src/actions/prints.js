e5c130d5c15accb2b76ba6805e5f387443560909
Chris Jativa <cjativa@barnesfoundation.org>
2023-11-07 08:28:24 -0500

    import ui from config

--- src/actions/prints.js
+++ src/actions/prints.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import * as ActionTypes from '../constants';
+import { ui } from '../../shared/config';
 
-const PRINTS_ENDPOINT = process.env.REACT_APP_PRINTS_ENDPOINT;
+const PRINTS_ENDPOINT = ui.printsEndpoint;
 
 const setPrints = (prints) => {
   return {
