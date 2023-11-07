6109709a324c52d260621c095a671835dfcea8c0
Chris Jativa <cjativa@barnesfoundation.org>
2023-11-07 08:09:24 -0500

    add imports of parseObjects and ui config from shared folder

--- src/routeWrapper.jsx
+++ src/routeWrapper.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import CommonWrap from './CommonWrap';
+import { ui } from '../shared/config';
+
 const ReactGA = require('react-ga');
 
-ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
+ReactGA.initialize(ui.googleAnalyticsID);
 
 // with advice from https://github.com/ReactTraining/react-router/issues/4278
 const routeWrapper = (WrappedComponent) => {
