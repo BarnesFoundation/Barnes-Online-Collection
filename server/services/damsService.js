11ab49e2fb0135ec183f51169d95703b77e7b5d4
Chris Jativa <cjativa@barnesfoundation.org>
2023-11-13 11:45:46 -0500

    run eslint on server code

--- server/services/damsService.js
+++ server/services/damsService.js
@@ -5,64 +5,70 @@ const NETX_API_TOKEN = process.env.NETX_API_TOKEN;
 const NETX_ENABLED = process.env.NETX_ENABLED || false;
 
 function generateGetAssetQuery(objectId) {
-    return {
-        jsonrpc: "2.0",
-        id: `GET_ASSETS_BY_QUERY_${objectId}`,
-        method: "getAssetsByQuery",
-        params: [
-            {
-                query: [
-                    // Query statement is to scope the search within our "Collection Website API" folder in NetX
-                    {
-                        operator: "and",
-                        folder: {
-                            folderId: 646,
-                            recursive: false,
-                        },
-                    },
-                    {
-                        operator: "and",
-                        exact: {
-                            attribute: "ObjectID (TMS)",
-                            value: objectId,
-                        },
-                    },
-                ],
+  return {
+    jsonrpc: "2.0",
+    id: `GET_ASSETS_BY_QUERY_${objectId}`,
+    method: "getAssetsByQuery",
+    params: [
+      {
+        query: [
+          // Query statement is to scope the search within our "Collection Website API" folder in NetX
+          {
+            operator: "and",
+            folder: {
+              folderId: 646,
+              recursive: false,
             },
-            {
-                sort: {
-                    field: "name",
-                    order: "asc",
-                },
-                page: {
-                    startIndex: 0,
-                    size: 10,
-                },
-                data: ["asset.id", "asset.base", "asset.attributes", "asset.file", "asset.proxies"],
+          },
+          {
+            operator: "and",
+            exact: {
+              attribute: "ObjectID (TMS)",
+              value: objectId,
             },
+          },
         ],
-    };
+      },
+      {
+        sort: {
+          field: "name",
+          order: "asc",
+        },
+        page: {
+          startIndex: 0,
+          size: 10,
+        },
+        data: [
+          "asset.id",
+          "asset.base",
+          "asset.attributes",
+          "asset.file",
+          "asset.proxies",
+        ],
+      },
+    ],
+  };
 }
 
 async function getAssetByObjectId(objectId) {
-    // In case we want to disable interaction with NetX for now
-    if (NETX_ENABLED === false) {
-        return [];
-    }
+  // In case we want to disable interaction with NetX for now
+  if (NETX_ENABLED === false) {
+    return [];
+  }
 
-    const response = await axios({
-        baseURL: NETX_BASE_URL,
-        url: '/api/rpc',
-        method: 'POST',
-        headers: {
-            Authorization: `apiToken ${NETX_API_TOKEN}`
-        },
-        data: generateGetAssetQuery(objectId)
-    });
+  const response = await axios({
+    baseURL: NETX_BASE_URL,
+    url: "/api/rpc",
+    method: "POST",
+    headers: {
+      Authorization: `apiToken ${NETX_API_TOKEN}`,
+    },
+    data: generateGetAssetQuery(objectId),
+  });
 
-    return response.data.result.results;
+  return response.data.result.results;
 }
 
 module.exports = {
-    getAssetByObjectId,
+  getAssetByObjectId,
 };
--- server/utils/graphCmsClient.js
+++ server/utils/graphCmsClient.js
@@ -1,5 +1,5 @@
 const { GraphQLClient } = require("graphql-request");
 
-const graphQLClient = new GraphQLClient(process.env.GRAPHCMS_ENDPOINT)
+const graphQLClient = new GraphQLClient(process.env.GRAPHCMS_ENDPOINT);
 
 module.exports = graphQLClient;
