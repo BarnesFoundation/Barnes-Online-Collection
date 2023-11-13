ae4a547d70f32dc4073e14fce3176fe7d9524f14
Chris Jativa <cjativa@barnesfoundation.org>
2023-11-13 11:48:07 -0500

    run linting on scripts

--- scripts/write-search-assets.js
+++ scripts/write-search-assets.js
@@ -1,34 +1,38 @@
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path')
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 
 const apiUrl = "https://collection.barnesfoundation.org/api/get-search-assets";
-const constantsDirectory = path.resolve(__dirname, '../public/resources');
+const constantsDirectory = path.resolve(__dirname, "../public/resources");
 
 const logError = (errorMsg) => {
-	console.log('Oops, Something went wrong while trying to build the search assets.')
-	console.log(errorMsg)
-}
+  console.log(
+    "Oops, Something went wrong while trying to build the search assets."
+  );
+  console.log(errorMsg);
+};
 
 const fetchSearchAssets = () => {
-	return axios.get(apiUrl).catch((error) => {
-		logError(error)
-	})
-}
+  return axios.get(apiUrl).catch((error) => {
+    logError(error);
+  });
+};
 
 const writeSearchAssetsFile = (text, onSuccess) => {
-	const pathName = path.join(constantsDirectory, "searchAssets.json");
-	fs.writeFile(pathName, text, (error) => {
-		if (error) {
-			return logError(error);
-		}
+  const pathName = path.join(constantsDirectory, "searchAssets.json");
+  fs.writeFile(pathName, text, (error) => {
+    if (error) {
+      return logError(error);
+    }
 
-		onSuccess();
-	})
-}
+    onSuccess();
+  });
+};
 
-fetchSearchAssets().then(response => {
-	writeSearchAssetsFile(response.data, () => {
-		console.log("Search assets generated in public/resources/searchAssets.json 👍")
-	})
-})
+fetchSearchAssets().then((response) => {
+  writeSearchAssetsFile(response.data, () => {
+    console.log(
+      "Search assets generated in public/resources/searchAssets.json 👍"
+    );
+  });
+});
