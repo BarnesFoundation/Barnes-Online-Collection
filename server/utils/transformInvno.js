4c24f0b25e20d1d2fee63f1ed80255fd5bcaf17c
Chris Jativa <cjativa@barnesfoundation.org>
2025-02-17 12:24:03 -0500

    better function doc.

--- server/utils/transformInvno.js
+++ server/utils/transformInvno.js
@@ -1,5 +1,8 @@
 /** Replaces the Object Number `.` character with `_`
  * since that's how the folders named by Object Number are formatted in NetX
+ *
+ * For example, transforms `01.01.01` to `01-01-01`
+ * @param objectNumber - The original object number using period characters
  */
 function transformInvno(objectNumber) {
   return objectNumber.replace(/\./g, "_");
