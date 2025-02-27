982516cbe804f5bd27d3d5e3f77674e5c23490af
Chris Jativa <cjativa@barnesfoundation.org>
2025-02-27 16:00:36 -0500

    add transform util

--- server/utils/transformInvno.js
+++ server/utils/transformInvno.js
@@ -8,6 +8,17 @@ function transformInvno(objectNumber) {
   return objectNumber.replace(/\./g, "_");
 }
 
+/** Replaces the NetX Object Number `_` character with `.`
+ * since that's how the folders named by Object Number are formatted in NetX
+ *
+ * For example, transforms `01_01_01` to `01.01.01`
+ * @param objectNumber - The NetX object number using underscore characters
+ */
+function transformNetXObjectNumber(objectNumber) {
+  return objectNumber.replace(/_/g, ".");
+}
+
 module.exports = {
   transformInvno,
+  transformNetXObjectNumber,
 };
